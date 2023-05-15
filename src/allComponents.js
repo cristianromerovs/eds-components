d9f85263e06726298f2d5fe6f88fa8fbd5acdef7
Cristian Eduardo Romero Valdes <mut_cromero@externos.entel.cl>
2023-05-15 16:53:20 -0400

    test 2

--- src/allComponents.js
+++
@@ -1,7 +0,0 @@
-import ButtonPrimary from './components/buttons/primaryButton'
-import Breadcrumb from './components/breadcrumbs/breadcrumb'
-
-export default { ButtonPrimary, Breadcrumb }
-
-customElements.define("eds-button", ButtonPrimary);
-customElements.define("eds-breadcrumb", Breadcrumb);
